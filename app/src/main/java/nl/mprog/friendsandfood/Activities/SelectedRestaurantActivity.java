0b3eec459c09e20bef994cdef9c9a6c04b219765
tcjverburg <tom_verburg@hotmail.nl>
2017-02-02 01:13:03 +0100

    Refractoring and fixed bug in OwnReviewsActivity.

--- app/src/main/java/nl/mprog/friendsandfood/Activities/SelectedRestaurantActivity.java
+++ app/src/main/java/nl/mprog/friendsandfood/Activities/SelectedRestaurantActivity.java
@@ -47,6 +47,8 @@ public class SelectedRestaurantActivity extends BaseActivity implements View.OnC
     private String restaurantName;
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference mRefFriends;
+
     private ArrayList<String> mFriendsCompleteNames = new ArrayList<>();
     private ArrayList<String> mFriendsID = new ArrayList<>();
     private ArrayList<String> allReviewIDs = new ArrayList<>();
@@ -76,6 +78,8 @@ public class SelectedRestaurantActivity extends BaseActivity implements View.OnC
         findViewById(R.id.submit).setOnClickListener(this);
         name.setText(restaurantName);
 
+        mRefFriends = database.getReference("users").child(profile).child("friends");
+        
         getUserFriendsValueEventListener();
         clickSelectReviewFriend();
         setToggleButton();
@@ -111,7 +115,6 @@ public class SelectedRestaurantActivity extends BaseActivity implements View.OnC
 
     /** First EventListener which gets all the friends of the user and calls addFriendInformation */
     public void getUserFriendsValueEventListener(){
-        DatabaseReference mRefFriends = database.getReference("users").child(profile).child("friends");
         mRefFriends.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
